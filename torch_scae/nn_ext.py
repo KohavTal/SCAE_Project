8e2d6f201dd882cda87b84e1892f346331b09334
KohavTal <kohavtal26@gmail.com>
2020-09-24 23:14:39 +0300

    Batch Normalization

--- torch_scae/nn_ext.py
+++ torch_scae/nn_ext.py
@@ -38,7 +38,7 @@ def BestStack(in_channels,
                          stride=stride)
         layers.append(conv)
         layers.append(activation())
-        layers.append(nn.Dropout(p=dropout))
+        layers.append(nn.BatchNorm2d(out_channels))
 
     if not activate_final:
         layers.pop()
